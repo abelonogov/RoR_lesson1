abcf0bb8a77aa6c6683448a43c839822d6e4b94a
Alexander Belonogov <acebel@mail.ru>
2014-10-19 22:52:09 +0300

    Lesson 1 task 1

--- 1.rb
+++ 1.rb
@@ -2,8 +2,5 @@ puts "Please enter numbers:"
 number_string = gets.chomp
 
 sum = 0
-number_string.each_char do |i|
-  sum += i.to_i
-end
-
+number_string.each_char {|i| sum += i.to_i}
 puts "The sum of entered numbers is #{sum}"
